0400b0775b2ad610d4a1b4c1b3e546875f605f2b
Mohamed Ali <kahano243@gmail.com>
2024-07-13 20:56:17 +0200

    refactoring some codes

--- obj/Debug/net8.0/StripePayment_Integration_WebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/StripePayment_Integration_WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StripePayment_Integration_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9adfb2c6b8c0e129ab6c23f6af64ef2e3d7d876c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e8cd9a9c2e450dea86c7820efadf11c0a994f99")]
 [assembly: System.Reflection.AssemblyProductAttribute("StripePayment_Integration_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StripePayment_Integration_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
